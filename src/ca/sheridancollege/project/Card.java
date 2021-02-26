e1dede6e35dc50efb2bb13619fa1934d4b7ba437
zaffdom <zaffidom@sheridancollege.ca>
2021-02-26 14:27:00 -0500

    test commit

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,6 +2,7 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
+ * oof
  */
 package ca.sheridancollege.project;
 
